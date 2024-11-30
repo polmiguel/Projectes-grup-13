45f10807f1348fdf66fd1ce3abec134d34b33900
Israel Llagostera <israel.llagostera4@gmail.com>
2024-11-30 10:14:00 +0100

    main_data

--- Dep_Prop/batteries.m
+++ Dep_Prop/batteries.m
@@ -10,6 +10,9 @@ S = 36.3;    % Wing area in m^2
 % Initial mass without batteries or fuel
 m0 = 8000; % kg
 
+% Combustion engines
+eff_comb = 0.2684;
+
 % Batteries
 E_bat = 11.1; % kWh per battery
 m_bat = 48;   % kg per battery
@@ -24,6 +27,12 @@ SFC = 0.408; % kg/kWh
 refrigerant_ratio = 0.10; % Coolant volume/battery volume ratio
 densidad_refrigerante = 1070; % kg/m^3
 
+% Generators
+P_generator = 400e3; % Power of Safran generator in W (400 kW)
+eff_generator = 0.85; % Efficiency of the generator (85%)
+m_generator = 34; % Mass of each generator in kg
+N_generators = 2; % Number of generators (one per turboprop)
+
 % Function for air density as a function of altitude
 rho = @(h) 1.225 * (1 - (0.0065 * h) / 288.15).^4.2561;
 
@@ -31,8 +40,8 @@ rho = @(h) 1.225 * (1 - (0.0065 * h) / 288.15).^4.2561;
 v_taxi = 7.5;           % Taxi speed in m/s
 t_taxi_out = 12.3 * 60; % Taxi-out time in seconds
 mu = 0.02;              % Rolling friction coefficient
-v_cruise = 600 / 3.6;   % Cruise speed in m/s
-h_cruise = 6000;        % Cruise altitude in m
+v_cruise = 500 / 3.6;   % Cruise speed in m/s
+h_cruise = 7600;        % Cruise altitude in m
 
 % Takeoff and landing
 theta_to = deg2rad(8);  % Climb angle in radians
@@ -85,8 +94,13 @@ while abs(m - m_prev) > tol && iter < max_iter
     D_cruise = 0.5 * rho(h_cruise) * v_cruise^2 * S * C_D;
     T_cruise = D_cruise; % Level flight
     P_cruise = T_cruise * v_cruise;
+
+    % Hybrid cruise power (including generator power)
+    P_hybrid = P_cruise/eff_comb; % Total power required during hybrid cruise
+    E_cruise_h = P_hybrid * (t_cruise_h - t_cruise_e); % Hybrid cruise energy
+
+    % Electric cruise energy
     E_cruise_e = P_cruise * t_cruise_e; % Electric cruise energy
-    E_cruise_h = P_cruise * t_cruise_h; % Hybrid cruise energy
 
     % Descent
     v_land_vals = linspace(v_cruise, 62, 1000); % Speed during descent
@@ -100,27 +114,34 @@ while abs(m - m_prev) > tol && iter < max_iter
     E_taxi_in = P_taxi * t_taxi_out;
 
     %% Total energies
-    E_elec = E_taxi_out + E_to + E_cruise_e + E_land + E_taxi_in; % Electric
-    E_hybrid = E_taxi_out + E_to + E_cruise_h + E_land + E_taxi_in; % Hybrid
-    E_comb = E_hybrid - E_elec; % Fuel energy
+    E_elec = E_taxi_out + E_to + E_cruise_e + E_land + E_taxi_in; % Total electric energy (J)
+    E_total = E_taxi_out + E_to + E_cruise_h + E_land + E_taxi_in; % Total energy required (J)
 
-    %% Mass and volume
-    % Batteries
+    % Energy from generators during hybrid cruise
+    E_gen = P_generator * N_generators * t_cruise_h * eff_generator; % Total energy from generators (J)
+
+    %% Fuel consumption
+    % Total power required from engines during hybrid cruise
+    P_engines_hybrid = P_hybrid; % Already includes generator power input
+    m_comb_cruise = SFC * (P_engines_hybrid / 1000) * (t_cruise_h / 3600); % Fuel mass during hybrid cruise (kg)
+
+    % Fuel mass is calculated based on the power required and SFC
+    m_comb = m_comb_cruise; % Total fuel mass (kg)
+    V_comb = m_comb / d_comb; % Fuel volume (L)
+
+    %% Batteries
     E_elec_kWh = E_elec / (1000 * 3600); % Convert energy to kWh
-    N_bat = ceil(E_elec_kWh / E_bat);   % Number of batteries
+    N_bat = ceil(E_elec_kWh / E_bat);    % Number of batteries
     m_bat_tot = N_bat * m_bat;
     V_bat_tot = N_bat * V_bat;
 
+    %% Mass and volume
     % Coolant
     V_refrigerante = V_bat_tot * refrigerant_ratio;
     m_refrigerante = V_refrigerante * densidad_refrigerante;
 
-    % Fuel
-    m_comb = SFC * E_comb / (3.6e6); % Fuel mass (using SFC)
-    V_comb = m_comb / d_comb;       % Fuel volume
-
     % Total mass
-    m = m0 + m_bat_tot + m_refrigerante + m_comb;
+    m = m0 + m_bat_tot + m_refrigerante + m_comb + (N_generators * m_generator);
 
     fprintf('Iteration %d: m = %.2f kg, m_bat_tot = %.2f kg, m_comb = %.2f kg\n', iter, m, m_bat_tot, m_comb);
 end
@@ -136,5 +157,6 @@ fprintf('Final mass: %.2f kg\n', m);
 fprintf('Number of batteries: %d\n', N_bat);
 fprintf('Fuel mass: %.2f kg\n', m_comb);
 fprintf('Fuel volume: %.2f L\n', V_comb);
+fprintf('Energy from generators: %.2f kWh\n', E_gen / (1000 * 3600));
 fprintf('Average power during takeoff: %.2f kW\n', P_to_avg / 1000); % Convert to kW
 fprintf('Average power during landing: %.2f kW\n', P_land_avg / 1000); % Convert to kW
